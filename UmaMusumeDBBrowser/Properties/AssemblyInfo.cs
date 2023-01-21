88f0a20290b95077756fc49b1f1ae013fe207e46
starik222 <starik222@mail.ru>
2023-01-21 22:33:02 +0900

    Обновление 1.5.8

--- UmaMusumeDBBrowser/Properties/AssemblyInfo.cs
+++ UmaMusumeDBBrowser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("vnfast.ru")]
 [assembly: AssemblyProduct("UmaMusumeDBBrowser")]
-[assembly: AssemblyCopyright("Copyright © vnfast.ru 2021")]
+[assembly: AssemblyCopyright("Copyright © vnfast.ru 2021-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.7.0")]
-[assembly: AssemblyFileVersion("1.5.7.0")]
+[assembly: AssemblyVersion("1.5.8.0")]
+[assembly: AssemblyFileVersion("1.5.8.0")]
